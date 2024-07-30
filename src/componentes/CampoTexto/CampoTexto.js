f3e218e565de4181924e61ecaae115e439981e71
Yuri Cavalieri <yurimagalhaes2004@gmail.com>
2024-07-30 11:29:17 -0300

    Inserção do evento onChange no input do Campo Texto

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,9 +1,18 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
+
+
+    let valor = 'Yuri'
+
+    const aoDigitado = (evento) => {
+        console.log(evento.target.value)
+        valor= evento.target.value
+        console.log(valor)
+    }
     return(
         <div className="campo-texto" >
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder= {props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder= {props.placeholder} />
         </div>
     )
 }
